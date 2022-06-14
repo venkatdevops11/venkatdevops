7db9c8f7f65919eef0a6bf257e43bcee1158640b
venkatdevops11 <yvramesh522@gmail.com>
2022-06-14 15:32:28 +0530

    this is my second commit

--- login.java
+++ login.java
@@ -1,6 +1,5 @@
 Hi, Welcome to devops
 This is the first file i am creating as part of devops
-today i am going to school
 i will pick my kid by 12:30PM
 i will complete my learning by month end
 thanks for all the supporters who provided git vidoes
